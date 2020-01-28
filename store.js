4125d62fcf7d959502ea554e2113efb615bbe312
Ryan Cwynar <ryan@fueledonbacon.com>
2020-01-27 23:06:11 -0500

    remove a function and consolidate a method

--- store.js
+++ store.js
@@ -11,6 +11,7 @@ class Tweet{
   }
 
   has(attr){
+
     return Array.isArray(this[attr]) && this[attr].length > 0
   } 
 
@@ -33,6 +34,7 @@ class Tweet{
   }
 
   static parseEmojis(text){
+
     if(!text) return []
     return text.match(/(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|[\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|[\ud83c[\ude32-\ude3a]|[\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g) || []
   }
@@ -43,6 +45,7 @@ const MAX_TWEETS_IN_MEMORY = 1000
 class TweetStore {
 
   constructor(){
+
     this.currIndex = -1
     this.tweets = []
     this.emojis = {}
@@ -61,6 +64,7 @@ class TweetStore {
   }
 
   updateCounts(tweet){
+
     this.totalTweetsProcessed++
     if(tweet.has('hashtags')) this.tweetsWith.hashtags++
     if(tweet.has('domains')) this.tweetsWith.domains++
@@ -74,10 +78,12 @@ class TweetStore {
   }
  
   nextIndex(){
+
     return (this.currIndex + 1) % MAX_TWEETS_IN_MEMORY 
   }
 
   pushTweet(tweet){
+
     this.currIndex = this.nextIndex()
     if(this.tweets.length < MAX_TWEETS_IN_MEMORY){
       this.tweets.push(tweet)
@@ -87,10 +93,12 @@ class TweetStore {
   }
 
   percentTweetsWith(type){
+
     return this.percent(this.tweetsWith[type])
   }
 
   percent(n){
+
     return Math.round(n / this.totalTweetsProcessed * 100)
   }
 
@@ -104,6 +112,7 @@ class TweetStore {
   }
 
   add(type, tweet){
+
     tweet[type].map(key => {
       if(this[type][key]){
         this[type][key] += 1
@@ -115,10 +124,13 @@ class TweetStore {
 
   earliestTweetTimestamp(){
 
+    let tweet
     if(this.tweets.length < MAX_TWEETS_IN_MEMORY)
-      return this.tweets[0]
+      tweet = this.tweets[0]
     else 
-      return this.tweets[this.nextIndex()].timestamp
+      tweet = this.tweets[this.nextIndex()]
+
+    return tweet.timestamp
   }
 
   latestTweetTimestamp(){
@@ -150,13 +162,6 @@ class TweetStore {
     return Math.round( timeframe * sample / (latest - earliest) )
   }
 
-  static outputTopItems(arr, limit){
-
-    arr = arr.slice(0, limit)
-
-    return arr.map(item=>item.name)
-  }
-
   static getTopItems(arr){
 
     let n = Object.keys(arr).length
@@ -175,8 +180,10 @@ class TweetStore {
   top(type, limit = 10){
 
     let arr = TweetStore.getTopItems(this[type])
-  
-    return TweetStore.outputTopItems(arr, limit)
+
+    return arr
+      .slice(0, limit)
+      .map(item=>item.name)
   }
 }
 
